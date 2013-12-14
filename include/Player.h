34c31dedb3663a1cb9ece49be58d0dad861248ea
Alexander Örn <alexanderorn1@gmail.com>
2013-12-14 20:49:28 +0100

    Addad a player class

---
+++ include/Player.h
@@ -0,0 +1,18 @@
+#include <Entity.h>
+#ifndef GAME_PLAYER_H
+#define GAME_PLAYER_H
+
+class Player : public Entity
+{
+public:
+	//Normal functions
+	virtual void Update(Game * p_pGame, float p_deltaTime);
+	virtual void Collide(Game * p_pGame, const Entity * p_pOther);
+	
+	// Get Functions
+	virtual sf::Sprite * GetSprite( ) const;
+	virtual eType GetType ( ) const;
+private:
+};
+
+#endif
