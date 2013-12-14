018163276e534c9ba28899f4745950a80f2defcc
MinO93 <MinO88_pro@hotmail.com>
2013-12-14 17:17:28 +0100

    Lagt till Tile Classen

---
+++ include/Tile.h
@@ -0,0 +1,28 @@
+#ifndef GAME_TILE_H
+#ifndef GAME_TILE_H
+#include <SFML\Graphics.hpp>
+
+class Tile
+{
+
+public:
+
+	Tile();
+	~Tile();
+
+	// Get Functions
+	virtual sf::Sprite GetSprite( ) const = 0;
+	
+
+
+	// Set Functions
+	
+
+private:
+
+
+
+};
+
+
+
