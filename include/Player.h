3d33d4672b0647c3c11d158784de4b2c96540488
Alexander Örn <alexanderorn1@gmail.com>
2013-12-14 22:22:09 +0100

    added movement

--- include/Player.h
+++ include/Player.h
@@ -2,18 +2,31 @@
 #define GAME_PLAYER_H
 
 #include <Entity.h>
+#include <Animation.h>
+#include <SFML/Window.hpp>
 
 class Player : public Entity
 {
 public:
+
+	Player(sf::Vector2f position);
+
 	//Normal functions
 	virtual void Update(Game * p_pGame, float p_deltaTime);
 	virtual void Collide(Game * p_pGame, const Entity * p_pOther);
+	void Attack();
 	
 	// Get Functions
 	virtual sf::Sprite * GetSprite( ) const;
 	virtual Entity::eType GetType ( ) const;
 private:
+	sf::Vector2f m_Postition;
+	Animation* m_Idle;
+	Animation* m_WalkUp;
+	Animation* m_WalkDown;
+	Animation* m_WalkLeft;
+	Animation* m_WalkRight;
+	Animation* m_CurrentAnimation;
 };
 
 #endif
--- source/Player.cpp
+++ source/Player.cpp
@@ -1,8 +1,49 @@
 #include <Player.h>
 #include <MemoryLeak.h>
 
+static float PLAYERSPEED = 2;
+
+Player::Player(sf::Vector2f position):
+	m_Postition(position),
+	m_Idle(new Animation("Data/Textures/idle.png", 200, 2)),
+	m_WalkUp(new Animation("Data/Textures/walk_up.png", 200, 2)),
+	m_WalkDown(new Animation("Data/Textures/walk_down.png", 200, 2)),
+	m_WalkLeft(new Animation("Data/Textures/walk_left.png", 200, 2)),
+	m_WalkRight(new Animation("Data/Textures/walk_right.png", 200, 2)),
+	m_CurrentAnimation(m_Idle)
+{
+
+}
+
 void Player::Update(Game * p_pGame, float p_deltaTime)
 {
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		m_Postition.y-=PLAYERSPEED;
+		m_CurrentAnimation = m_WalkUp;
+	}
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+		m_Postition.y+=PLAYERSPEED;
+		m_CurrentAnimation = m_WalkDown;
+	}
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		m_Postition.x+=PLAYERSPEED;
+		m_CurrentAnimation = m_WalkRight;
+	}
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		m_Postition.x-=PLAYERSPEED;
+		m_CurrentAnimation = m_WalkLeft;
+	}
+
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+	{
+		// Attack();
+	}
+
+
 
 }
 
@@ -13,7 +54,7 @@ void Player::Collide(Game * p_pGame, const Entity * p_pOther)
 
 sf::Sprite* Player::GetSprite( ) const
 {
-	return NULL;
+	return m_CurrentAnimation->getSprite();
 }
 
 Entity::eType Player::GetType( ) const
