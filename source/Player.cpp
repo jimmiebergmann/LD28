2174b959673e984419bbceba3545cdd2f17a3e1c
Alexander Örn <alexanderorn1@gmail.com>
2013-12-14 22:51:34 +0100

    Andrade i Player.cp

--- source/Player.cpp
+++ source/Player.cpp
@@ -38,6 +38,8 @@ void Player::Update(Game * p_pGame, float p_deltaTime)
 		m_CurrentAnimation = m_WalkLeft;
 	}
 
+	m_CurrentAnimation->getSprite.setPosition(m_Postition);
+
 	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 	{
 		// Attack();
