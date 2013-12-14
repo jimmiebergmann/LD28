eb5fafeceed024e221b7e7ea6232f8aa85b08d81
Thism2 <thism.rasse@hotmail.com>
2013-12-14 18:20:07 +0100

    Adderade MemoryLeak.h (andvänd newAPI och inte new)

--- source/GameImp.cpp
+++ source/GameImp.cpp
@@ -1,24 +1,21 @@
 #include <GameImp.h>
 #include <Config.h>
+#include <MemoryLeak.h>
 
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Event.hpp>
 
 #include <iostream>
 
-#define newAPI new
-
 //private global vairables
 static const sf::String g_title = "Y.O.G.O. Game";
-static unsigned int g_width = 200;
-static unsigned int g_heigth = 300;
 static sf::RenderWindow * g_pRenderWindow = 0;
 
 //private global functions
-static void g_HandleEvent(GameImp & p_gameImp, sf::Event p_event);
-static bool g_Load();
-static void g_Unload();
-static void g_ErrorLoad(sf::String & p_errorDesc);
+static void HandleEvent(GameImp & p_gameImp, sf::Event p_event);
+static bool Load();
+static void Unload();
+static void ErrorLoad(sf::String & p_errorDesc);
 
 GameImp::GameImp( )
 {
@@ -26,31 +23,31 @@ GameImp::GameImp( )
 
 GameImp::~GameImp( )
 {
-	g_Unload();
 }
 
 int GameImp::Run( )
 {
-	if(!g_Load()) {
-		g_ErrorLoad(sf::String("Could not load resouced"));
+	InitMemoryLeak();
+
+	if(!Load()) {
+		ErrorLoad(sf::String("Could not load resouced"));
 		return 1;
 	}
-
-	g_pRenderWindow = newAPI sf::RenderWindow(sf::VideoMode(g_width, g_heigth), g_title);
 
 	while (g_pRenderWindow->isOpen())
 	{
 		sf::Event eventA;
 		while (g_pRenderWindow->pollEvent(eventA))
 		{
-			g_HandleEvent(*this, eventA);
+			HandleEvent(*this, eventA);
 		}
 	}
+	Unload();
 	delete g_pRenderWindow;
 	return 0;
 }
 
-static void g_HandleEvent(GameImp & p_gameImp, sf::Event p_event) {
+static void HandleEvent(GameImp & p_gameImp, sf::Event p_event) {
 	switch (p_event.type)
 	{
 	case sf::Event::Closed:
@@ -61,26 +58,27 @@ static void g_HandleEvent(GameImp & p_gameImp, sf::Event p_event) {
 	}
 }
 
-static bool g_Load() {
-	if( Config::Load( "Data/Configs.txt" ) == false )
+static bool Load() {
+	if( Config::Load( "Data/Configs1.txt" ) == false )
 	{
-		return 0;
+		return false;
 	}
-	
+	sf::Vector2u size = Config::GetScreenSize(); 
+	g_pRenderWindow = newAPI sf::RenderWindow(sf::VideoMode(size.x, size.y), g_title);
+
 	return true;
 }
 
-static void g_Unload() {
-
+static void Unload() {
 
 }
 
-static void g_ErrorLoad(sf::String & p_errorDesc) {
+static void ErrorLoad(sf::String & p_errorDesc) {
 	char val;
 	std::cout << "Error: " << p_errorDesc.toAnsiString() << std::endl;
 	std::cout << "Press any key to conture " << std::endl;
 	std::cin >> val;
-	g_Unload();
+	Unload();
 }
 
 Game::EntityVector & GameImp::getEntities() {
