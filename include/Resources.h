c8da1608a7ef0698182f9de3a0ad1299f237f1da
Alexander Örn <alexanderorn1@gmail.com>
2013-12-14 17:35:52 +0100

    Added resources class

---
+++ include/Resources.h
@@ -0,0 +1,35 @@
+#ifndef GAME_RESOURCES_H
+#define GAME_RESOURCES_H
+
+#include <SFML\Audio.hpp>
+#include <SFML\Graphics.hpp>
+#include <unordered_map>
+#include <string>
+
+
+class Resources
+{
+public:
+
+	static sf::Sound * GetSound( const std::string & filename );
+	static sf::Texture * GetTexture( const std::string & filename );
+	static void UnloadSounds();
+	static void UnloadTextures();
+	static void UnloadResources();
+
+private:
+
+	struct SoundData
+	{
+		sf::SoundBuffer * buffer;
+		sf::Sound * sound;
+	};
+
+	typedef std::unordered_map< std::string, SoundData > SoundMap;
+	static SoundMap s_Sounds;
+
+	typedef std::unordered_map< std::string, sf::Texture* > TextureMap;
+	static TextureMap s_Textures;
+
+};
+#endif
---
+++ source/Resources.cpp
@@ -0,0 +1,78 @@
+#include "Resources.h"
+#include <iostream>
+
+Resources::SoundMap Resources::s_Sounds;
+
+sf::Sound * Resources::GetSound( const std::string &filename )
+{
+	SoundMap::iterator it = s_Sounds.find( filename );
+
+	if( it != s_Sounds.end( ) )
+	{
+		return it->second.sound;
+	}
+
+
+	sf::SoundBuffer * buffer = new sf::SoundBuffer;
+	if( buffer->loadFromFile( filename ) == false )
+	{
+		std::cout << "Failed to load sound file." << std::endl;
+		return NULL;
+	}
+
+	sf::Sound * sound = new sf::Sound( );
+	sound->setBuffer( *buffer );
+
+	SoundData data;
+	data.buffer = buffer;
+	data.sound = sound;
+	s_Sounds[ filename ] = data;
+
+	return sound;
+}
+
+sf::Texture * Resources::GetTexture( const std::string &filename )
+{
+	TextureMap::iterator it = s_Textures.find( filename );
+
+	if( it != s_Textures.end( ) )
+	{
+		return it->second;
+	}
+
+	sf::Texture * texture = new sf::Texture;
+	if( texture->loadFromFile( filename ) == false )
+	
+	{
+		std::cout << "Failed to load sound file." << std::endl;
+		return NULL;
+	} 
+	else 
+	{
+		return texture;
+	}
+}
+
+void Resources::UnloadSounds()
+{
+	for(SoundMap::iterator it = s_Sounds.begin(); it != s_Sounds.end(); it++)
+	{
+		delete it->second.buffer;
+		delete it->second.sound;
+	}
+
+}
+
+void Resources::UnloadTextures()
+{
+	for(TextureMap::iterator it = s_Textures.begin(); it != s_Textures.end(); it++)
+	{
+		delete it->second;
+	}
+}
+
+void Resources::UnloadResources()
+{
+	UnloadSounds();
+	UnloadTextures();
+}
