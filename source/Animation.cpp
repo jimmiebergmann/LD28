b8565c9a8198bb5931dde8f68db41e741dde96fb
Alexander Örn <alexanderorn1@gmail.com>
2013-12-14 20:07:35 +0100

    Added animation class

---
+++ source/Animation.cpp
@@ -0,0 +1,54 @@
+#include "Animation.h"
+
+Animation::Animation(const std::string& filename, int timePerFrame, int numFrames) : 
+	m_timePerFrame(timePerFrame),
+	m_numFrames(numFrames),
+	m_currentFrame(0)
+{
+	m_texture = *Resources::GetTexture(filename);
+	m_sprite.setTexture(m_texture);
+
+	// The rectangle below is used as an "view" for viewing only part of the sprites texture. Parameters are x position, y position, width and height. The width is divided by m_numFrames, this is the number of pixels per frame.
+	sf::IntRect textureRect(0, 0, m_texture.getSize().x/m_numFrames, m_texture.getSize().y);
+	m_sprite.setTextureRect(textureRect);
+}
+
+Animation::~Animation()
+{
+}
+
+void Animation::update()
+{
+	// If timer is higher than the limit, switch to next frame
+	if(m_frameTimer.getElapsedTime().asMilliseconds() > m_timePerFrame)
+	{
+		// Restart the timer
+		m_frameTimer.restart();
+
+		m_currentFrame++;
+
+		// Wrap around if we're at the end of the sprite-sheet
+		if(m_currentFrame >= m_numFrames)
+		{
+			m_currentFrame = 0;
+		}
+
+		// Get a copy of the old texture rect
+		sf::IntRect currentRect = m_sprite.getTextureRect();
+		// Set x-position (left) of the rectangle. The assignment gives 0 for the first frame, 1*width for the second etc.
+		currentRect.left = currentRect.width * m_currentFrame;
+
+		m_sprite.setTextureRect(currentRect);
+	}
+}
+
+void Animation::setPosition(const sf::Vector2f& position)
+{
+	m_sprite.setPosition(position);
+}
+
+const sf::Sprite& Animation::getSprite() const
+{
+	return m_sprite;
+}
+
