721387ea106387d19d2c806b99e30eb74536d068
Thism2 <thism.rasse@hotmail.com>
2013-12-14 20:22:52 +0100

    Rabbit + Entity impelmenterad

---
+++ source/Rabbit.cpp
@@ -0,0 +1,38 @@
+#include <Rabbit.h>
+#include <Resources.h>
+
+#include <MemoryLeak.h>
+
+Rabbit::Rabbit(const sf::Vector2f p_position)
+{
+
+}
+Rabbit::~Rabbit()
+{
+
+}
+
+void Rabbit:: Update(Game * p_pGame, float p_deltaTime)
+{
+
+}
+
+void Rabbit::Collide(Game * p_pGame, const Entity * p_pOther)
+{
+	switch (p_pOther->GetType())
+	{
+	default:
+		break;
+	}
+}
+
+Entity::eType Rabbit::GetType() const 
+{
+	return Entity::eType::Type_Rabit;
+}
+
+sf::Sprite * Rabbit:: GetSprite( ) const
+{
+	sf::Sprite * pSprite = new sf::Sprite(*Resources::GetTexture("Rabbit.png"));
+	return pSprite;
+}
