a69d60bd7be0a25acc258f6e3d464d783c174117
Thism2 <thism.rasse@hotmail.com>
2013-12-15 16:50:44 +0100

    zoom gameImp fix

--- source/Tent.cpp
+++ source/Tent.cpp
@@ -8,6 +8,7 @@ Tent::Tent(const sf::Vector2f p_position) :
 	m_pSprite(new sf::Sprite(*Resources::GetTexture("Data/Textures/Tent.png"))) 
 {
 	m_pSprite->setPosition(p_position);
+	m_pSprite->scale( 2.0f, 2.0f);
 }
 
 Tent::~Tent()
