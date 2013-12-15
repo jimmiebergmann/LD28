a5caf3280a09769e73942b301957cdf305d51006
MinO93 <MinO88_pro@hotmail.com>
2013-12-15 14:36:45 +0100

    Fisk

--- source/Rabbit.cpp
+++ source/Rabbit.cpp
@@ -7,20 +7,18 @@
 #include <MemoryLeak.h>
 
 Rabbit::Rabbit(const sf::Vector2f p_position) :
-<<<<<<< HEAD
 	m_pSprite(new sf::Sprite(*Resources::GetTexture("Data/Textures/Rabbit.png"))) 
-=======
-	m_position(p_position),
-	m_pSprite(new sf::Sprite(*Resources::GetTexture("Data/Textures/RabbitTest.png"))) 
->>>>>>> f2b631d7e78b879648ed6cb3716dc81375549669
 {
 	m_pSprite->setPosition(p_position);
 }
 
 Rabbit::~Rabbit()
 {
-	delete m_pSprite;
-	m_pSprite = 0;
+	if( m_pSprite )
+	{
+		delete m_pSprite;
+		m_pSprite = NULL;
+	}
 }
 
 void Rabbit::Update(Game * p_pGame, float p_deltaTime)
